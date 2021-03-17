b65dc6ea0c4fa170ec28e0a25895a6a7b1624348
Ilman Hamzatov <iha@objektkultur.de>
2021-03-17 10:20:40 +0100

    update 3.0

--- okbatterie_libary/lib/okbatterie_libary.dart
+++ okbatterie_libary/lib/okbatterie_libary.dart
@@ -1,24 +1,18 @@
 library okbatterie_libary;
 
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
-/// A Calculator.
-class myTestLabel extends StatelessWidget {
-  Widget _textSection() => Container(
-        padding: const EdgeInsets.all(32),
-        color: Colors.blue[200],
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Text',
-            )
-          ],
-        ),
-      );
+class Batterylabel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _textSection();
+    return Container(
+      padding: const EdgeInsets.all(32),
+      color: Colors.blue[200],
+      child: Column(
+        children: <Widget>[
+          Text('Text'),
+        ],
+      ),
+    );
   }
 }
