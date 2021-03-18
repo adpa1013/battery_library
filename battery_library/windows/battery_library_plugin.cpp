d0e07b164b8b9970ac69aff691748914a322249c
Paul Adam <pad@objektkultur.de>
2021-03-18 14:56:39 +0100

    fixed code formating issues

--- battery_library/windows/battery_library_plugin.cpp
+++ battery_library/windows/battery_library_plugin.cpp
@@ -16,65 +16,72 @@
 #include <string.h>
 #include <iostream>
 
-namespace {
+namespace
+{
 
-class BatteryLibraryPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
+  class BatteryLibraryPlugin : public flutter::Plugin
+  {
+  public:
+    static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
 
-  BatteryLibraryPlugin();
+    BatteryLibraryPlugin();
 
-  virtual ~BatteryLibraryPlugin();
+    virtual ~BatteryLibraryPlugin();
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
+  private:
+    // Called when a method is called on this plugin's channel from Dart.
+    void HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue> &method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+  };
 
-// static
-void BatteryLibraryPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "battery_library",
-          &flutter::StandardMethodCodec::GetInstance());
+  // static
+  void BatteryLibraryPlugin::RegisterWithRegistrar(
+      flutter::PluginRegistrarWindows *registrar)
+  {
+    auto channel =
+        std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+            registrar->messenger(), "battery_library",
+            &flutter::StandardMethodCodec::GetInstance());
 
-  auto plugin = std::make_unique<BatteryLibraryPlugin>();
+    auto plugin = std::make_unique<BatteryLibraryPlugin>();
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+    channel->SetMethodCallHandler(
+        [plugin_pointer = plugin.get()](const auto &call, auto result) {
+          plugin_pointer->HandleMethodCall(call, std::move(result));
+        });
 
-  registrar->AddPlugin(std::move(plugin));
-}
-
-BatteryLibraryPlugin::BatteryLibraryPlugin() {}
-
-BatteryLibraryPlugin::~BatteryLibraryPlugin() {}
-
-void BatteryLibraryPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    SYSTEM_POWER_STATUS spsPwr;
-	if (GetSystemPowerStatus(&spsPwr))
-	{
-    std::string battery_state = std::to_string(static_cast<double>(spsPwr.BatteryLifePercent));
-    result->Success(flutter::EncodableValue(battery_state));
-  } 
-    
-  } else {
-    result->NotImplemented();
+    registrar->AddPlugin(std::move(plugin));
   }
-}
 
-}  // namespace
+  BatteryLibraryPlugin::BatteryLibraryPlugin() {}
+
+  BatteryLibraryPlugin::~BatteryLibraryPlugin() {}
+
+  void BatteryLibraryPlugin::HandleMethodCall(
+      const flutter::MethodCall<flutter::EncodableValue> &method_call,
+      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+  {
+    if (method_call.method_name().compare("getPlatformVersion") == 0)
+    {
+      SYSTEM_POWER_STATUS spsPwr;
+      if (GetSystemPowerStatus(&spsPwr))
+      {
+        std::string battery_state = std::to_string(static_cast<double>(spsPwr.BatteryLifePercent));
+        result->Success(flutter::EncodableValue(battery_state));
+      }
+    }
+    else
+    {
+      result->NotImplemented();
+    }
+  }
+
+} 
 
 void BatteryLibraryPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
+    FlutterDesktopPluginRegistrarRef registrar)
+{
   BatteryLibraryPlugin::RegisterWithRegistrar(
       flutter::PluginRegistrarManager::GetInstance()
           ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
