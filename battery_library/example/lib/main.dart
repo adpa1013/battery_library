04fb9ece9716d33abdb3a8091be09a10abae2e9a
Alexandros Agiasotis <aag@objektkultur.de>
2021-03-19 12:31:41 +0100

    Add a battery load plugin and view current percentage in an app

--- battery_library/example/lib/main.dart
+++ battery_library/example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:battery_library/battery_library.dart';
 
 void main() {
@@ -14,31 +13,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  int _batteryLevel = -1;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    updateBatteryLevel();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
+  Future<void> updateBatteryLevel() async {
+    int batteryLevel;
     try {
-      platformVersion = await BatteryLibrary.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
+      batteryLevel = await BatteryLibrary.batteryLevel;
+    } on Exception {
+      batteryLevel = -1;
     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
     setState(() {
-      _platformVersion = platformVersion;
+      _batteryLevel = batteryLevel;
     });
   }
 
@@ -49,8 +41,12 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Column(
+          children: [
+            Center(
+              child: Text('Power left: $_batteryLevel'),
+            ),
+          ],
         ),
       ),
     );
--- battery_library/lib/battery_library.dart
+++ battery_library/lib/battery_library.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class BatteryLibrary {
   static const MethodChannel _channel = const MethodChannel('battery_library');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<int> get batteryLevel async {
+    final int batteryLevel = await _channel.invokeMethod('getBatteryLevel');
+    return batteryLevel;
   }
 }
